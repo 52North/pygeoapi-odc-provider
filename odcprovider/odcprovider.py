acaaaa3555b178caadf0457edd4d331ba50af7f4
Martin Pontius <m.pontius@52north.org>
2021-06-30 14:37:19 +0200

    Update source file header

--- odcprovider/odcprovider.py
+++ odcprovider/odcprovider.py
@@ -1,12 +1,31 @@
 # =================================================================
+# Copyright (C) 2021-2021 52°North Spatial Information Research GmbH
 #
-# Authors: Martin Pontius
+# This program is free software; you can redistribute it and/or modify it
+# under the terms of the GNU General Public License version 2 as published
+# by the Free Software Foundation.
 #
-# Copyright (c) 2021 Martin Pontius (52°North)
+# If the program is linked with libraries which are licensed under one of
+# the following licenses, the combination of the program with the linked
+# library is not considered a "derivative work" of the program:
 #
-# ...
+#     - Apache License, version 2.0
+#     - Apache Software License, version 1.0
+#     - GNU Lesser General Public License, version 3
+#     - Mozilla Public License, versions 1.0, 1.1 and 2.0
+#     - Common Development and Distribution License (CDDL), version 1.0
 #
-# Based on pygeoapi's RasterioProvider (rasterio_.py)
+# Therefore the distribution of the program linked with libraries licensed
+# under the aforementioned licenses, is permitted by the copyright holders
+# if the distribution is compliant with both the GNU General Public
+# License version 2 and the aforementioned licenses.
+#
+# This program is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General
+# Public License for more details.
+#
+# Based on pygeoapi's RasterioProvider (https://github.com/geopython/pygeoapi/blob/master/pygeoapi/provider/rasterio_.py)
 #
 # =================================================================
 
