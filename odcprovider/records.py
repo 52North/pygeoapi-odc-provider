f96bd4cdddae494b21249e5618e7079cd0f49bf6
EHJ-52n <e.h.juerrens@52north.org>
2021-07-22 21:55:49 +0200

    Start implementing OpenDataCubeRecordsProvider.query(...)

--- odcprovider/records.py
+++ odcprovider/records.py
@@ -15,6 +15,7 @@ import logging
 
 import datacube
 from pygeoapi.provider.base import (BaseProvider,
+                                    ProviderGenericError,
                                     ProviderConnectionError,
                                     ProviderQueryError,
                                     ProviderItemNotFoundError)
@@ -24,6 +25,8 @@ LOGGER = logging.getLogger(__name__)
 class OpenDataCubeRecordsProvider(BaseProvider):
     """
     OGC API Records provider for an OpenDataCube instance
+
+    Each OpenDataCube product family is one collection, hence we have one record per collection per dataset
     """
 
     def __init__(self, provider_def):
@@ -39,4 +42,68 @@ class OpenDataCubeRecordsProvider(BaseProvider):
 
         self.dc = datacube.Datacube(app='pygeoapi_provider')
 
+        products = [d['name'] for d in self.dc.list_products(with_pandas=False)]
+
+        if self.data not in products:
+            raise ProviderGenericError("Configured product '{}' is not contained in OpenDataCube instance")
+
         LOGGER.debug("Provider initiated: name: '{}', type: '{}', data: '{}'".format(self.name, self.type, self.data))
+
+    def query(self, startindex=0, limit=10, resulttype='results',
+              bbox=[], datetime_=None, properties=[], sortby=[],
+              select_properties=[], skip_geometry=False, q=None, **kwargs):
+        """
+        query TinyDB document store
+        :param startindex: starting record to return (default 0)
+        :param limit: number of records to return (default 10)
+        :param resulttype: return results or hit limit (default results)
+        :param bbox: bounding box [minx,miny,maxx,maxy]
+        :param datetime_: temporal (datestamp or extent)
+        :param properties: list of tuples (name, value)
+        :param sortby: list of dicts (property, order)
+        :param select_properties: list of property names
+        :param skip_geometry: bool of whether to skip geometry (default False)
+        :param q: full-text search term(s)
+        :returns: dict of 0..n GeoJSON feature collection
+        """
+
+        # {
+        # 'id': 1,
+        # 'name': 'dsm__MB__The_Pas_2014',
+        # 'description': '"dsm" data created by "MB" within the project "The_Pas_2014"',
+        # 'creation_time': None,
+        # 'format': None,
+        # 'label': None,
+        # 'lat': None,
+        # 'lon': None,
+        # 'time': None,
+        # 'platform': None,
+        # 'instrument': None,
+        # 'region_code': None,
+        # 'product_family': None,
+        # 'dataset_maturity': None,
+        # 'crs': 'EPSG:2957',
+        # 'spatial_dimensions': ('y', 'x'),
+        # 'tile_size': None,
+        # 'resolution': (-1.0, 1.0)
+        # }
+        #
+        # TODO woher den Spatial Extend des Products?
+        # TODO datasets oder measurements auflisten? tendiere zu measurements
+        # TODO woher weiß ich, in welcher Collection ich bin und daher, welches Produkt ich nehmen muss? z.Zt. Config -> data
+        # self.data
+
+        if limit < 1:
+            raise ProviderQueryError("limit < 1 makes no sense!")
+
+        product = self.dc.index.products.get_by_name(self.data)
+
+        measurements = list(filter(lambda d: d['product'] in self.data,
+                                   self.dc.list_measurements(with_pandas=False)))
+
+        feature_collection = {
+            'type': 'FeatureCollection',
+            'features': []
+        }
+
+        return feature_collection
